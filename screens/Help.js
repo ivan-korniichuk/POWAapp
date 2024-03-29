590ba67fe15df4d11f4d129b6e4c0be2abb65ca3
diyani22 <diyanimodi22@gmail.com>
2024-03-29 21:36:48 +0000

    Update Help.js

---
+++ screens/Help.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Help = ({navigation}) => {
+  return (
+    <View
+      style={{
+        height: 50,
+        padding: 20,
+        backgroundColor: 'blue',
+      }}>
+    </View>
+  );
+};
+
+export default Help;
