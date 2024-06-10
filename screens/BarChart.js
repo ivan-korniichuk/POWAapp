89ff3ec13d0092ecaaf15edb52c4862789c603eb
Ivan Korniichuk <85750192+ivan-korniichuk@users.noreply.github.com>
2024-06-10 23:29:07 +0100

    fixed an error

--- screens/BarChart.js
+++ screens/BarChart.js
@@ -1,19 +1,67 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { View} from 'react-native';
-=======
 import { React, useState } from 'react';
 import { View, ScrollView, Text, StyleSheet } from 'react-native';
 import { DefaultButton, TimeSelector, BarChartComp, ToogleButton, ColoredToggleButton } from '../components/index';
 import { ArrowLeft } from 'react-native-feather';
 import { BarChartStyles } from '../styles/index.style';
->>>>>>> Stashed changes
 
 const BarChart = ({navigation}) => {
+  const [barValues, setBarValues] = useState([0,0,0,0]);
+  const [features, setFeatures] = useState(["","",""]);
+
+  function updateBarValues (newValues) {
+    if (newValues.length === 4) {
+      console.log('update')
+      setFeatures(analyseValues(newValues))
+      setBarValues(newValues)
+    }
+  }
+
+  function analyseValues(values) {
+    const sortedArray = values.slice().sort((a, b) => a - b);
+
+    if (sortedArray[0] < 0) {
+      var lowestIndex = values.indexOf(sortedArray[0]);
+    }
+
+    if (sortedArray[3] > 0) {
+      var highestIndex = values.indexOf(sortedArray[3]);
+    }
+
+    const closestToZeroValue = values.reduce((closest, current) =>
+        Math.abs(current) < Math.abs(closest) ? current : closest
+    );
+
+    var zeroIndex = values.indexOf(closestToZeroValue);
+
+    function getColumn (index) {
+      switch (index) {
+        case 0:
+          return "Perspective"
+        case 1:
+          return "Other Centred"
+        case 2:
+          return "Willingness to Learn"
+        case 3:
+          return "Accurate Self-Assessment"
+        default:
+          return ""
+      }
+    }
+
+    return [getColumn(zeroIndex), getColumn(highestIndex), getColumn(lowestIndex)];
+  }
+
+  function onSelect(buttonName) {
+    // 
+    updateBarValues([
+      Math.random() * 20 - 10,
+      Math.random() * 20 - 10,
+      Math.random() * 20 - 10,
+      Math.random() * 20 - 10])
+    // 
+    console.log(buttonName);
+  }
   return (
-<<<<<<< Updated upstream
-    <View>
-=======
     <View style={BarChartStyles.container}>
       <TimeSelector onSelect={( buttonName ) => {onSelect(buttonName)}}/>
 
@@ -33,9 +81,27 @@ const BarChart = ({navigation}) => {
 
       <DefaultButton icon={<ArrowLeft color='#ffffff'/>} text='Back' onTouch={() => navigation.navigate('Home')}/>
 
->>>>>>> Stashed changes
     </View>
   );
 };
 
 export default BarChart;
+
+styles = StyleSheet.create({
+  progress: {
+    flexDirection: 'row',
+    width: '100%',
+    borderBottomWidth: 1,
+    paddingVertical: 12,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  text: {
+    fontSize: 20,
+    fontFamily: 'JosefinSans_500Medium',
+  },
+  line: {
+    paddingVertical: 12,
+  },
+});
