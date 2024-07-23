1c2e77bf1e328a0f333a093d694f04f99772f8ea
Ivan Korniichuk <85750192+ivan-korniichuk@users.noreply.github.com>
2024-07-23 15:53:11 +0100

    deleted some styles

--- styles/Graph.style.js
+++ styles/Graph.style.js
@@ -8,44 +8,12 @@ export default StyleSheet.create({
       flex: 1,
       padding: 20,
     },
-    text: {
-      fontSize: 18,
-      fontFamily: 'JosefinSans_300Light',
-      textAlign: 'center',
-    },
-    textBoxRow: {
-      flexDirection: 'row', 
-      height: screenHeight * 0.5 * 0.11,
-      width: '25%',
-    },
-    barsRow: {
-      position: 'absolute',
-      flexDirection: 'row', 
-      width: '25%',
-      height: '100%',
-    },
-    bar: {
-      height: 0,
-      width: '100%',
-      backgroundColor: "rgba(255,255,255,0.65)",
-    },
     textBox: {
       height: '100%',
       width: '100%',
       justifyContent: 'center',
       alignItems: 'center',
     },
-    // chart: {
-    //   // flex: 1,
-    //   height: screenHeight * 0.5 * 0.33,
-    //   backgroundColor: COLORS.primary
-    // },
-    // mainButton: {
-    //     // bottom: 0,
-    // },
-    // rightBorder: {
-    //     borderRightWidth: 1,
-    // },
     mainText: {
         color: COLORS.primary,
         fontFamily: 'JosefinSans_700Bold',
