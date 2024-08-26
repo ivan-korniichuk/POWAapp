a8025e5f78495169892e3a43f7aad576ed2d2495
Shay Patel <shay@shay.services>
2024-08-26 13:17:43 +0100

    Log relevent day's data in console

--- screens/Calendar.js
+++ screens/Calendar.js
@@ -1,29 +1,65 @@
+import { COLORS } from '../constants/index';
+
 import { React, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { DefaultButton, TimeSelector, ColoredToggleButton, ProgressGraphComp } from '../components/index';
 import { GraphStyles, ScrollViewStyles } from '../styles/index.style';
 import { ArrowLeft } from 'react-native-feather';
 import { useData } from '../storage/storageService';
-import { getAllTimeStats, getLastMonthStats, getLastWeekStats } from '../utils/graphUtils';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Calendar } from 'react-native-calendars';
+import moment from 'moment';
+
+const DOT_TYPES = {
+  report: { color: 'orange' },
+}
 
 const CalendarScreen = ({navigation}) => {
   const { reports } = useData();
-  const [ selectedDay, setSelectedDay ] = useState('');
-  
+  const [ selectedDay, setSelectedDay ] = useState(null);
+  const [ markedDates, setMarkedDates ] = useState({});
+
+  useEffect(() => {
+    let newMarkedDates = {};
+    if (selectedDay != null) {
+      newMarkedDates[selectedDay.dateString] = {selected: true, dots: [], disableTouchEvent: true, selectedColor: COLORS.primary}
+    };
+    for (let report of reports) {
+      let reportDate = moment(report.dateCreatedCli).format("YYYY-MM-DD");
+      if (Object.keys(newMarkedDates).includes(reportDate)) {
+        newMarkedDates[reportDate].dots.push(DOT_TYPES.report);
+      } else {
+        newMarkedDates[reportDate] = {dots: [DOT_TYPES.report]};
+      }
+    }
+    setMarkedDates(newMarkedDates);
+    console.log(newMarkedDates);
+  }, [reports, selectedDay])
+
+  useEffect(() => {
+    if (selectedDay == null) return;
+    console.log(selectedDay);
+    let filteredReports = reports.filter(report => moment(report.dateCreatedCli).format("YYYY-MM-DD") == selectedDay.dateString).sort((a, b) => a.dateCreatedCli - b.dateCreatedCli);
+    console.log(filteredReports);
+    console.log("===");
+  }, [reports, selectedDay])
+
   return (
     <View style={GraphStyles.container}>
 
-      <Calendar
-        onDayPress={day => {
-          setSelectedDay(day.dateString);
-        }}
-        markedDates={{
-          [selectedDay]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-        }}
-      ></Calendar>
+      <ScrollView contentContainerStyle={ScrollViewStyles.scrollViewContent} style={ScrollViewStyles.scrollView}>
 
-      <DefaultButton containerStyle={styles.topMarginButton} icon={<ArrowLeft color='#ffffff'/>} text='Back' onTouch={() => navigation.navigate('Home')}/>
+        <Calendar
+          onDayPress={day => {
+            setSelectedDay(day);
+          }}
+          markingType={'multi-dot'}
+          markedDates={markedDates}
+          enableSwipeMonths={true}
+        ></Calendar>
+
+        <DefaultButton containerStyle={styles.topMarginButton} icon={<ArrowLeft color='#ffffff'/>} text='Back' onTouch={() => navigation.navigate('Home')}/>
+
+      </ScrollView>
 
     </View>
   );
